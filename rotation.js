fe062bcef71166b34a8b8a8b93254592837533fe
Sachin976 <83978822+Sachin976@users.noreply.github.com>
2021-05-11 00:02:17 +0530

    Update rotation.js

--- rotation.js
+++ rotation.js
@@ -1,7 +1,9 @@
-let string1 = "ABCD"
-let string2 = "CDAB"
-let string4 = "ACDB"
-let string3 = "hellosachin"
+//to check whether two strings are rotation of each other or not
+
+let string1 = "ABCD";
+let string2 = "CDAB";
+let string4 = "ACDB";
+let string3 = "hellosachin";
 
 //length function
 const len = (string) =>{
@@ -15,9 +17,9 @@ const len = (string) =>{
 const cut = (string,start,end) =>{
     let new_string = ""; 
     for(let i = start;i <=end; i++ ){
-        new_string += string[i]
+        new_string += string[i];
     }
-    return new_string
+    return new_string;
 }
 
 //function to check strings rotation
@@ -25,20 +27,20 @@ const rotation = (string_1,string_2) =>{
     if(len(string_1) === len(string_2)){
         let i = 0;
         for(i; i<=len(string_1); i++){
-            string_2 = cut(string_2,1,len(string_2)-1) + string_2[0]
+            string_2 = cut(string_2,1,len(string_2)-1) + string_2[0];
             if(string_1 === string_2){
-                console.log("Congrats!! Strings are rotation of each other")
-                break
+                console.log("Congrats!! Strings are rotation of each other");
+                break;
             }
         }
         if(i > len(string_1)){
-            console.log("Sorry!! Strings are not rotation of each other")
+            console.log("Sorry!! Strings are not rotation of each other");
         }
     }else{
-        console.log("Sorry!! Strings are not rotation of each other")
+        console.log("Sorry!! Strings are not rotation of each other");
     }
 }
 
-rotation(string1,string2)
-rotation(string1,string3)
-rotation(string1,string4)
+rotation(string1,string2);
+rotation(string1,string3);
+rotation(string1,string4);
