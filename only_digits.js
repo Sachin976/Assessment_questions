5e16be45dbc67f79fe276db3c60096da5ccc85fa
Sachin976 <83978822+Sachin976@users.noreply.github.com>
2021-05-10 23:57:56 +0530

    Update only_digits.js

--- only_digits.js
+++ only_digits.js
@@ -1,3 +1,5 @@
+//To check if there are only digits in a given string
+
 const string_1 = "hello@1234";
 const string_2 = "12346481379";
 
@@ -12,14 +14,13 @@ const len = (string) =>{
 
 const check_only_digits = (string) =>{
     for(let i = 0; i < len(string); i++){
-        let temp = string[i]
+        let temp = string[i];
         if(!"1234567890".includes(temp)){
-            console.log("String contains characters")
-            return
+            console.log("String contains characters");
+            return;
         }   
     }
-    console.log("String only contains digits")
+    console.log("String only contains digits");
 }
-
-check_only_digits(string_1)
-check_only_digits(string_2)
+check_only_digits(string_1);
+check_only_digits(string_2);
